c78de260aa07d56ef1edd02ba500224a4b3c0a3c
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2022-05-12 10:57:41 +0200

    Update detect-os-and-uninstall.sh
    ensure delete files of clamav install

--- scripts/detect-os-and-uninstall.sh
+++ scripts/detect-os-and-uninstall.sh
@@ -4,12 +4,24 @@ detect=$(cat /etc/*-release | grep 'ID=')
 if [[ $detect == *"debian"* ]]; then
   export DEBIAN_FRONTEND=noninteractive
 	sudo apt-get --assume-yes remove clamav
+	rm -rf /var/log/clamav || true
+        rm -rf /var/lib/clamav || true
+	rm -rf /usr/share/clamav || true
 elif [[ $detect == *"ubuntu"* ]]; then
   export DEBIAN_FRONTEND=noninteractive
 	sudo apt-get --assume-yes remove clamav
+	rm -rf /var/log/clamav || true
+        rm -rf /var/lib/clamav || true
+	rm -rf /usr/share/clamav || true
 elif [[ $detect == *"centos"* ]]; then
-    sudo yum remove -y epel-release 
-    sudo yum remove -y clamav 
+    	sudo yum remove -y epel-release 
+    	sudo yum remove -y clamav
+	rm -rf /var/log/clamav || true
+        rm -rf /var/lib/clamav || true
+	rm -rf /usr/share/clamav || true
 elif [[ $detect == *"alpine"* ]]; then
 	sudo apk del clamav
+	rm -rf /var/log/clamav || true
+        rm -rf /var/lib/clamav || true
+	rm -rf /usr/share/clamav || true
 fi
